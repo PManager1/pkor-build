9849e6211d638d25c3dd701c36917177afa781e5
jpca999 <jpca999@gmail.com>
2020-01-07 11:03:22 -0700

    build copied

--- README.md
+++
@@ -1,1 +0,0 @@
-# pkor-build
---
+++ precache-manifest.b7a952310d841b6cdd8704f7cf4d8b43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1329adfbd4a892cd183967dfc410499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6274e14f5888c01e9282",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "e777a21f2aa773621cd3",
+    "url": "/static/css/main.3915d1e7.chunk.css"
+  },
+  {
+    "revision": "6274e14f5888c01e9282",
+    "url": "/static/js/2.5c628c58.chunk.js"
+  },
+  {
+    "revision": "3383dc4532941e428730f7233cf63feb",
+    "url": "/static/js/2.5c628c58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e777a21f2aa773621cd3",
+    "url": "/static/js/main.3974b650.chunk.js"
+  },
+  {
+    "revision": "5d185f9b63468364365a",
+    "url": "/static/js/runtime-main.53649389.js"
+  }
+]);
